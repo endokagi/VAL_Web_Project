b6595cd4b7260b49793ab5ee2016f9513a433519
Witthawat  Tanoi <38246228+endokagi@users.noreply.github.com>
2022-12-15 09:22:47 +0700

    prepair change to igasha github

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var path = require("path");
+var favicon = require('serve-favicon');
 
 var routes = require("./route");
 var app = express();
@@ -8,6 +9,8 @@ app.set("port",process.env.PORT || 8080);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.use(express.static("assets/img"));
+app.use(favicon(__dirname + '/VestALife.ico'));
 app.use(routes);
 
 app.listen(app.get("port"),function(){

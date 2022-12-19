c7a27c5c11a75630e1440baca3a557193a9cfea9
Witthawat  Tanoi <38246228+endokagi@users.noreply.github.com>
2022-12-19 17:32:47 +0700

    updated navbar to multi pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import img from './assets/img/money.jpg'
+import React from 'react';
 import './App.css';
-import { Button, Card, Row, Col, Container, Navbar, Nav, Jumbotron } from 'react-bootstrap';
+
+import Navbar from './pages/navbar';
+import { Routes, Route } from 'react-router-dom';
+
+// page //
+import Home from './pages/home';
+import About from './pages/about';
+import Cats from './pages/cats';
+
+
 function App() {
   return (
-    <div>
-      <Navbar className='justify-centent-between' bg='light' expanded='lg'>
-        <Navbar.Brand href='/'>
-          MONEY
-        </Navbar.Brand>
-        <div style={{ display: 'flex' }}>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='mr-auto'>
-              <Nav.Link href='/#'>Home</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-      </Navbar>
-      <Container style={{ padding: 20, marginTop: 20 }}>
-        <br />
-        <h1 style={{ textAlign: 'center' }}>MONEY อ้วงๆ</h1>
-        <br />
-
-        <row>
-          <Col md={4}>
-            <Card style={{ padding: 20, backgroundColor: 'salmon'}}>
-              <Card.Body>
-                <Card.Img variant='top' src= {img} />
-                <Card.Title>MONEY</Card.Title>
-                <Card.Text>อ้วงๆ</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </row>
-      </Container>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/cats' element={<Cats />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/cats.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+import img from '../assets/img/money.jpg';
+import { Container, Card, Row, Col } from "react-bootstrap";
+
+function Cats() {
+    return (
+        <Container style={{ padding: 20, marginTop: 20 }}>
+            <h1 style={{ textAlign: 'center' }}>Cats</h1>
+            <br />
+
+            <Row>
+                <Col md={4}>
+                    <Card style={{ padding: 20, backgroundColor: 'salmon' }}>
+                        <Card.Body>
+                            <Card.Img variant='top' src={img} />
+                            <Card.Title>MONEY</Card.Title>
+                            <Card.Text>อ้วงๆ</Card.Text>
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
+};
+
+export default Cats;

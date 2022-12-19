f5f7cf0265127692db4e9fae10b62cdfb61d6a0a
Baibua1210 <56666179+Baibua1210@users.noreply.github.com>
2022-12-19 17:33:10 +0700

    update navbar

--- src/App.js
+++ src/App.js
@@ -1,39 +1,20 @@
-import logo from './assets/img/Angel.jpg';
+import React from 'react';
 import './App.css';
-import { Button, Card, Row, Col, Container, Navbar, Nav, Jumbotron } from 'react-bootstrap';
-import CardHeader from 'react-bootstrap/esm/CardHeader';
+import Home from './pages/Home';
+import About from './pages/About';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from './pages/Navbar';
+import { NavDropdown } from 'react-bootstrap';
+
 function App() {
   return (
     <div>
-      <Navbar className='justify-centent-between' bg='light' expanded='lg'>
-        <Navbar.Brand href='/'>
-          MONEY
-        </Navbar.Brand>
-        <div style={{ display: 'flex'}}>
-          <Navbar.Toggle aria-controls='basic-navbar-nav' />
-          <Navbar.Collapse id='basic-navbar-nav'>
-            <Nav className='mr-auto'>
-              <Nav.Link href='/https://www.youtube.com/watch?v=kpfisl0VFm4' target='_blank'>Let's go</Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </div>
-      </Navbar>
-      <Container style={{ padding: 20, marginTop: 20}}>
-        <br />
-          <h1 style={{textAlign: 'center'}}>Need money for cats</h1>
-        <br />
-        <Row>
-          <Col md={4}>
-            <Card style={{ padding: 20, backgroundColor: '#3A3C4B'}}>
-              <Card.Body>
-                <Card.Img variant='top' src={logo}/>
-                <Card.Title style={{color: '#fff'}}>Angel</Card.Title>
-                <Card.Text style={{color: '#fff'}}>Fat Cat</Card.Text>
-              </Card.Body>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
+    <Navbar/>
+    <NavDropdown/>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/about' element={<About/>}/>
+    </Routes>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from '../assets/img/Angel.jpg';
+import { Container, Row, Col, Card } from "react-bootstrap";
+function Home() {
+    return(
+        <div>
+           <Container style={{ padding: 20, marginTop: 20 }}>
+        <br />
+        <h1 style={{ textAlign: 'center' }}>Need money for cats</h1>
+        <br />
+        <Row>
+          <Col md={4}>
+            <Card style={{ padding: 20, backgroundColor: '#3A3C4B' }}>
+              <Card.Body>
+                <Card.Img variant='top' src={logo} />
+                <Card.Title style={{ color: '#fff' }}>Angel</Card.Title>
+                <Card.Text style={{ color: '#fff' }}>Fat Cat</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+        </div>
+    );
+}
+
+export default Home;
